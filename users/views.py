494c51f27837e3c389237d324c976fc5c2f989db
BAISALI D <bdas32691@gmail.com>
2024-08-23 09:32:12 +0530

    login commit

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 from users.forms import *
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login
+
+from django.urls import reverse
+
+
+
 
 def registration(request):
     UFDO=UserForm()
@@ -36,3 +42,29 @@ def registration(request):
             return HttpResponse('Not Valid')
      
     return render(request,'registration.html',d)
+
+#login -----
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:           
+            """
+            check
+            if it is active user then it is authorized or else invalid credentials.
+            AUO : User object
+            """
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+    return render(request,'userLogin.html')
